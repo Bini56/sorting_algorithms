2dcdb45eed0abd995a735860e5ac96ad6a177bf1
Binyam <biatsb5656@gmail.com>
2024-01-27 15:23:18 -0800

    writted a function that sorts an array of integers in ascending order using the Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,54 +1,52 @@
 #include "sort.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * counting_sort - sorts an array of integers in ascending order
- * using the Counting sort algorithm
- * @array: the array
- * @size: the size of the array
- *
- * Return: void
- */
+  * counting_sort - Afunction that sorts an array using counting algorithm.
+  * @array: The array to sort.
+  * @size: The length of the array.
+  * Return: Nothing.
+  */
 void counting_sort(int *array, size_t size)
 {
-	unsigned long i;
-	int j, max = 0;
-	int *count_array, *sorted_array;
+	unsigned int i = 1;
+	int *counter = NULL, k = 0, j = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	count_array = calloc(max + 1, sizeof(int));
-	if (!count_array)
-		return;
-
-	for (i = 0; i < size; i++)
-		count_array[array[i]] += 1;
-
-	for (j = 1; j < max + 1; j++)
-		count_array[j] += count_array[j - 1];
-
-	print_array(count_array, max + 1);
-
-	sorted_array = malloc(size * sizeof(int));
-	if (!sorted_array)
+	k = array[0];
+	for (; i < size; i++)
 	{
-		free(count_array);
-		return;
-	}
-	for (j = size - 1; j >= 0; j--)
-	{
-		i = count_array[array[j]];
-		sorted_array[i - 1] = array[j];
-		count_array[array[j]] -= 1;
+		if (array[i] > k)
+			k = array[i];
 	}
 
+	counter = malloc(sizeof(int) * (k + 1));
+	if (counter == NULL)
+		return;
+
+	for (j = 0; j <= k; j++)
+		counter[j] = 0;
 	for (i = 0; i < size; i++)
-		array[i] = sorted_array[i];
-	free(count_array);
-	free(sorted_array);
+		counter[array[i]] += 1;
+	for (j = 0; j < k; j++)
+	{
+		counter[j + 1] += counter[j];
+		printf("%d, ", counter[j]);
+	}
+	counter[j + 1] += counter[j];
+	printf("%d\n", counter[j + 1]);
+	for (i = 0; i < size; i++)
+	{
+		j = counter[array[i]] - 1;
+		if (array[i] != array[j])
+		{
+			k = array[i];
+			array[i] = array[j];
+			array[j] = k;
+		}
+	}
+	free(counter);
 }
